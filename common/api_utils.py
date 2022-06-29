dc1901dc0e6fa8b7eba6b370d09c3be70ac56cc5
zeloff <kirill@zelenin.info>
2022-06-29 21:23:33 +0300

    fix recursive import when app start by run main.py

--- common/api_utils.py
+++ common/api_utils.py
@@ -1,41 +1,40 @@
 import socket
-import main
 from common.config import logger
 
 
-def check_api_connectivity():
+def check_api_connectivity(settings):
     try:
         socket.setdefaulttimeout(3)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((main.settings.vulners_host, 443))
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((settings.vulners_host, 443))
         return True
     except socket.error as ex:
         logger.exception(ex)
         return False
 
 
-async def get_api_key_info():
-    vulners_request = main.session.build_request(
+async def get_api_key_info(session, settings):
+    vulners_request = session.build_request(
         method='GET',
-        url=f'https://{main.settings.vulners_host}/api/v3/apiKey/info/',
-        params={'apiKey': main.settings.vulners_api_key}
+        url=f'https://{settings.vulners_host}/api/v3/apiKey/info/',
+        params={'apiKey': settings.vulners_api_key}
     )
-    vulners_response = await main.session.send(vulners_request)
+    vulners_response = await session.send(vulners_request)
     return vulners_response.json().pop("data")
 
 
-async def get_cached_cost(license_type):
-    vulners_request = main.session.build_request(
+async def get_cached_cost(license_type, session, settings, statistics):
+    vulners_request = session.build_request(
         method='GET',
-        url=f'https://{main.settings.vulners_host}/api/v3/credit/get_requests_cost',
+        url=f'https://{settings.vulners_host}/api/v3/credit/get_requests_cost',
     )
     cost_data = None
-    vulners_response = await main.session.send(vulners_request)
+    vulners_response = await session.send(vulners_request)
     for license_costs in vulners_response.json()["data"]["costs"]:
         if license_costs['license'] != license_type:
             continue
         cost_data = license_costs['costs']
         break
     result = 0
-    for endpoint, count in list(main.statistics.items()):
+    for endpoint, count in list(statistics.items()):
         result += count * cost_data.get(endpoint, 0)
     return result
